f43027e39a8f12f16a61f38d1e006050f55e8891
eoohosun <redysj0517@naver.com>
2021-04-03 21:30:52 +0900

    use case, ERD 작성완료

---
+++ src/main/java/com/coupangEats/CoupangEatsApplication.java
@@ -0,0 +1,13 @@
+package com.coupangEats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoupangEatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoupangEatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coupangEats/CoupangEatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coupangEats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoupangEatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
